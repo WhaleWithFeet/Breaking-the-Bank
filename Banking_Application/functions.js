601fe3135555bd0b18d36fee8526e55b123cef3d
KHanson12 <konnerhanson02@gmail.com>
2024-10-16 21:23:23 -0400

    Add files via upload

--- Banking_Application/functions.js
+++ Banking_Application/functions.js
@@ -3,6 +3,9 @@ let accounts = [
     
 ];
 
+let checkingBalance = 0;
+let savingsBalance = 0;
+
 window.onload = function() {
     releaseStorage(); 
 };
@@ -41,10 +44,12 @@ function interperetSignUp(event){
     window.location.href = "login.html";
 }
 
-function addAccount(username, password){
+function addAccount(username, password, checkingBalance, savingsBalance){
     const temp_account = {
         username: username,
-        password: password
+        password: password,
+        checkingbalance: checkingBalance,
+        savingsbalance: savingsBalance
     };
     accounts.push(temp_account);
     storeAccounts();
@@ -59,4 +64,44 @@ function releaseStorage(){
     for(let i = 0; i < temp_account.length;i++){
         accounts.push(temp_account[i]);
     }
+}
+function checkCheckingBalance(accNum){
+    alert("Your current balance is $" + accounts[accNum].checkingBalance);
+    return;
+}
+function checkSavingsBalance(accNum){
+    alert("Your current balance is $" + accounts[accNum].savingsBalance);
+    return;
+}
+function depositChecking(accNum, amount){
+    if(amount >= 0.00 && Number(amount) == amount) {
+        accounts[accNum].checkingBalance += amount;
+        alert("Operation successful.");
+        return;
+    }
+    alert("Invalid operation: type error or invalid amount entered.");
+}
+function withdrawalChecking(accNum, amount){
+    if(amount >= 0.00 && Number(amount) == amount && amount <= checkingBalance) {
+        accounts[accNum].checkingBalance -= amount;
+        alert("Operation successful.");
+        return;
+    }
+    alert("Invalid operation: type error or invalid amount entered.");
+}
+function depositSavings(accNum, amount){
+    if(amount >= 0.00 && Number(amount) == amount) {
+        accounts[accNum].savingsBalance += amount;
+        alert("Operation successful.");
+        return;
+    }
+    alert("Invalid operation: type error or invalid amount entered.");
+}
+function withdrawalSavings(accNum, amount){
+    if(amount >= 0.00 && Number(amount) == amount && amount <= savingsBalance) {
+        accounts[accNum].savingsBalance -= amount;
+        alert("Operation successful.");
+        return;
+    }
+    alert("Invalid operation: type error or invalid amount entered.");
 }
