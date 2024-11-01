92a4e36e8638bc57c3388de3366e5e0457696fde
Philip Lussi <philiplussi@gmail.com>
2024-10-31 20:42:03 -0400

    Updated Financial Page
    Made it where you can choose a specific stock to analyze which can be updated in one browser session.

--- Banking_Application/functions.js
+++ Banking_Application/functions.js
@@ -188,9 +188,9 @@ function saveAccount() {
 //
 // API calls
 //
-async function getStockInfo(){
+async function getStockInfo(stock_name){
     try{
-        const response = await fetch('https://api.polygon.io/v2/aggs/ticker/AAPL/range/1/day/2023-01-09/2023-02-10?adjusted=true&sort=asc&apiKey=ynz12GyWGUsmiJni91lapm5j5guVpf5i');
+        const response = await fetch("https://api.polygon.io/v2/aggs/ticker/" + stock_name + "/range/1/day/2023-01-09/2023-02-10?adjusted=true&sort=asc&apiKey=ynz12GyWGUsmiJni91lapm5j5guVpf5i");
         if(!response.ok){
             alert("call did not go through")
         }
@@ -204,24 +204,32 @@ async function getStockInfo(){
 //
 // Stock Graph
 //
+let chart;
 document.addEventListener("DOMContentLoaded", function() {
-    window.ctx = document.getElementById('myChart').getContext('2d');
-    printStocks();
+    const ctx = document.getElementById('myChart').getContext('2d');
+    document.getElementById('create_graph').addEventListener('click', function() {
+        const stockTicker = document.getElementById('stock_ticker').value;
+        printStocks(stockTicker, ctx);
+    })
 });
 
-async function printStocks(){
-    const stocks = await getStockInfo();
+async function printStocks(stock_ticker, ctx){
+    const stocks = await getStockInfo(stock_ticker);
     const results = stocks ? stocks.results : [];      // Array of daily stock data
     
     const dates = results.map(item => new Date(item.t).toLocaleDateString());
     const prices = results.map(item => item.c);
 
-    new Chart(ctx, {
+    if(chart){
+        chart.destroy();
+    }
+
+    chart = new Chart(ctx, {
         type: 'line',
         data: {
             labels: dates,
             datasets: [{
-                label: "AAPL Stock Price",
+                label: stock_ticker + " Stock Price",
                 data: prices,
                 backgroundColor: "rgba(10, 20, 30, .2)",
                 borderColor: 'rgba(0, 0, 0, 1)',
